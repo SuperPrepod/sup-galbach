474016dd48e503b5affc6a5575acd7ae2e866f67
SuperPrepod <mark.sidorov.99@gmail.com>
2025-04-27 16:57:07 +0400

    Начало дипломааааааааааа

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/views/index.blade.php",
+      "./resources/**/*.js",
+      "./resources/css/app.css",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
