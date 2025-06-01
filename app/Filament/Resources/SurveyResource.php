ee5a776e53a662ed1cf29ccae126832321419d5f
SuperPrepod <mark.sidorov.99@gmail.com>
2025-06-01 15:30:25 +0400

    ababababa

---
+++ app/Filament/Resources/SurveyResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SurveyResource\Pages;
+use App\Filament\Resources\SurveyResource\RelationManagers;
+use App\Models\Survey;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SurveyResource extends Resource
+{
+    protected static ?string $model = Survey::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('FIO')
+                    ->required(),
+                Forms\Components\Textarea::make('Description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Toggle::make('Active')
+                    ->required(),
+                Forms\Components\TextInput::make('AGE')
+                    ->required()
+                    ->numeric(),
+                    Select::make('game_id')
+                    ->relationship('selected_game', 'Name')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('FIO')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('Active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('AGE')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('game_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSurveys::route('/'),
+            'create' => Pages\CreateSurvey::route('/create'),
+            'edit' => Pages\EditSurvey::route('/{record}/edit'),
+        ];
+    }
+}
